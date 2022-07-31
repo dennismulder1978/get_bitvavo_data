302e4c08717c255d126e33cb8c3e1ba5b87daaeb
Dennis Mulder <dnns.mldr@gmail.com>
2022-07-31 16:46:33 +0200

    clean-up v1.0

--- func.py
+++ func.py
@@ -1,6 +1,6 @@
 from Secret import const
 from python_bitvavo_api.bitvavo import Bitvavo
-
+from datetime import datetime
 
 bitvavo = Bitvavo({
     'APIKEY': const.api_key,
@@ -12,7 +12,33 @@ bitvavo = Bitvavo({
 })
 
 
+def period_punisher():
+    # determine UNIX-time in milliseconds last whole hour
+    # Create list start and stop UNIX-times per 28 dag periods.
+    # 2.419.200.000 milliseconds per period.
+    # 26 periods in 2 year time.
+
+    last_hour = datetime(
+        int(datetime.now().strftime('%Y')),  # current year
+        int(datetime.now().strftime('%m')),  # current month
+        int(datetime.now().strftime('%d')),  # current day
+        int(datetime.now().strftime('%H')),  # current hour
+        0,  # current hour, at 0 minutes, 0 seconds (start last hour)
+        0,  # current hour, at 0 minutes, 0 seconds (start last hour)
+    )
+
+    # to do: MAP function -2415600000 milliseconds in 26 periods list.
+    end = int(last_hour.timestamp()*1000)  # converted to milliseconds UNIX-time
+    return end
+
+
 def price_list(symbol: str):
+    # Hourly price per pair in periods of 28 days.
+    # 24 hours per day, 28 days per period (4 weeks)= 672 hours per period
     pair = str.upper(symbol) + '-EUR'
-    resp = bitvavo.candles(pair, '1m', {})
-    return [float(resp[x][4]) for x in range(1, len(resp))]
+    resp = bitvavo.candles(pair, '1h', {'limit': 672})
+    print(f'{pair} start: {resp[0][0]}')
+    print(f'{pair} end: {resp[671][0]}')
+    print(f'{pair} delta = {resp[0][0]-resp[671][0]}')
+
+    return [float(resp[x][4]) for x in range(0, len(resp))]
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from func import price_list
+from func import *
 import pandas as pd
 import numpy as np
 
 coin_list = ['BTC', 'ETH', 'SOL', 'ADA', 'MATIC', 'AVAX', 'SAND']
 coin_price_lst = np.array([price_list(each) for each in coin_list])
-# pd.DataFrame.from_records(coin_price_lst, index=coin_list).T.to_csv('name.csv')
+pd.DataFrame.from_records(coin_price_lst, index=coin_list).T.to_csv('name.csv')
 print(pd.DataFrame.from_records(coin_price_lst, index=coin_list).T)
+
+print(period_punisher())
