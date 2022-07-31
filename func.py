741cce584d1575f4f29be20a15abb2ad8513811c
Dennis Mulder <dnns.mldr@gmail.com>
2022-07-31 19:54:28 +0200

    expansion to 2 years! v1.1

--- func.py
+++ func.py
@@ -15,8 +15,6 @@ bitvavo = Bitvavo({
 def period_punisher():
     # determine UNIX-time in milliseconds last whole hour
     # Create list start and stop UNIX-times per 28 dag periods.
-    # 2.419.200.000 milliseconds per period.
-    # 26 periods in 2 year time.
 
     last_hour = datetime(
         int(datetime.now().strftime('%Y')),  # current year
@@ -28,17 +26,29 @@ def period_punisher():
     )
 
     # to do: MAP function -2415600000 milliseconds in 26 periods list.
-    end = int(last_hour.timestamp()*1000)  # converted to milliseconds UNIX-time
-    return end
+    period_list = []
+    timer = int(last_hour.timestamp()*1000)  # epoch time last whole hour in milliseconds
+    for i in range(0, 27):  # 26 periods in 2 year time.
+        period_list.append(timer)
+        timer -= 2419200000  # 2.419.200.000 milliseconds per period (28 days).
+    return period_list
 
 
 def price_list(symbol: str):
-    # Hourly price per pair in periods of 28 days.
-    # 24 hours per day, 28 days per period (4 weeks)= 672 hours per period
-    pair = str.upper(symbol) + '-EUR'
-    resp = bitvavo.candles(pair, '1h', {'limit': 672})
-    print(f'{pair} start: {resp[0][0]}')
-    print(f'{pair} end: {resp[671][0]}')
-    print(f'{pair} delta = {resp[0][0]-resp[671][0]}')
+    pair = str.upper(symbol) + '-EUR'   # determine pair
+    period_list = period_punisher()  # retrieving period_list per 28 days for 2 years
+    pair_list = []  # start with an empty list
+    time_list = []  # empty time-list for control
+    for i in range(1, len(period_list)):
+        # Hourly price per pair in periods of 28 days.
+        # 24 hours per day, 28 days per period (4 weeks)= 672 hours per period
+        print(f'{i} -- {datetime.fromtimestamp(period_list[i]/1000)} == {period_list[i]}')
 
-    return [float(resp[x][4]) for x in range(0, len(resp))]
+        resp = bitvavo.candles(pair, '1h', {'end': period_list[i], 'limit': 673})
+        print(datetime.fromtimestamp(resp[0][0]/1000))
+
+        for j in range(0, len(resp)-1):
+            pair_list.append(float(resp[j][4]))
+            time_list.append(datetime.fromtimestamp(int(resp[j][0] / 1000)))
+
+    return pair_list
--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from func import *
 import pandas as pd
 import numpy as np
 
-coin_list = ['BTC', 'ETH', 'SOL', 'ADA', 'MATIC', 'AVAX', 'SAND']
+'''
+Safe 2 years of hourly coin-market prices per coin. 
+'''
+
+# coin_list = ['BTC', 'ETH', 'SOL', 'ADA', 'MATIC', 'AVAX', 'SAND']
+coin_list = ['BTC']
 coin_price_lst = np.array([price_list(each) for each in coin_list])
 pd.DataFrame.from_records(coin_price_lst, index=coin_list).T.to_csv('name.csv')
 print(pd.DataFrame.from_records(coin_price_lst, index=coin_list).T)
-
-print(period_punisher())
