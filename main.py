b43b9aa7a3c88997c4fa7129b6cc051fcb6a0b4c
Dennis Mulder <dnns.mldr@gmail.com>
2022-07-31 21:05:28 +0200

    Expansion to 2 years! v1.1 Filled with 0.0 when data is absent.

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ coin_price_lst = np.array([price_list(each) for each in coin_list])
 
 
 # safe to file
-name = 'CoinPriceData__' + str(datetime.now().strftime('%d-%m-%Y_%H.%M.%S')) + '.csv'
+name = 'CoinPriceData__' + datetime.now().strftime('%d-%m-%Y_%H.%M.%S') + '.csv'
 pd.DataFrame.from_records(coin_price_lst, index=coin_list).T.to_csv(name)
