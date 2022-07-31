ab77e0cef3caafbd23adef7665650538db0be653
dennismulder1978 <dnns.mldr@gmail.com>
2022-07-31 11:29:30 +0200

    First working file v1.0

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from func import *
+from func import price_list
 import pandas as pd
 import numpy as np
 
-coin_list = ['BTC', 'ETH', 'ADA', 'MATIC', 'AVAX', 'SOL']
+coin_list = ['BTC', 'ETH', 'SOL', 'ADA', 'MATIC', 'AVAX', 'SAND']
 coin_price_lst = np.array([price_list(each) for each in coin_list])
-a = pd.DataFrame.from_records(coin_price_lst, index=coin_list)
-# create_csv(coin_price_lst)
-
-a.to_csv('test1.csv')
+pd.DataFrame.from_records(coin_price_lst, index=coin_list).T.to_csv('name.csv')
