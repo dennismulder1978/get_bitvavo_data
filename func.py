94ddb77b0a8b087c60476896f60fedca6bccdca7
Dennis Mulder <dnns.mldr@gmail.com>
2022-07-31 20:58:25 +0200

    Expansion to 2 years! v1.1 Filled with 0.0 when data is absent.

--- func.py
+++ func.py
@@ -1,3 +1,4 @@
+import numpy as np
 from Secret import const
 from python_bitvavo_api.bitvavo import Bitvavo
 from datetime import datetime
@@ -25,12 +26,11 @@ def period_punisher():
         0,  # current hour, at 0 minutes, 0 seconds (start last hour)
     )
 
-    # to do: MAP function -2415600000 milliseconds in 26 periods list.
     period_list = []
     timer = int(last_hour.timestamp()*1000)  # epoch time last whole hour in milliseconds
     for i in range(0, 27):  # 26 periods in 2 year time.
         period_list.append(timer)
-        timer -= 2419200000  # 2.419.200.000 milliseconds per period (28 days).
+        timer -= 2419200000  # 2.419.200.000 milliseconds per period (28 days)
     return period_list
 
 
@@ -38,17 +38,20 @@ def price_list(symbol: str):
     pair = str.upper(symbol) + '-EUR'   # determine pair
     period_list = period_punisher()  # retrieving period_list per 28 days for 2 years
     pair_list = []  # start with an empty list
-    time_list = []  # empty time-list for control
-    for i in range(1, len(period_list)):
+    for i in range(0, len(period_list)):
         # Hourly price per pair in periods of 28 days.
         # 24 hours per day, 28 days per period (4 weeks)= 672 hours per period
-        print(f'{i} -- {datetime.fromtimestamp(period_list[i]/1000)} == {period_list[i]}')
 
         resp = bitvavo.candles(pair, '1h', {'end': period_list[i], 'limit': 673})
-        print(datetime.fromtimestamp(resp[0][0]/1000))
+        if len(resp) == 673:
+            for j in range(1, len(resp)):
+ #               print(f'{i}-{j}: {resp[j][0]}')
+                pair_list.append(float(resp[j][4]))
+        else:
+            for j in range(1, 673):
+                pair_list.append(0)
 
-        for j in range(0, len(resp)-1):
-            pair_list.append(float(resp[j][4]))
-            time_list.append(datetime.fromtimestamp(int(resp[j][0] / 1000)))
-
+    #         time_list.append(datetime.fromtimestamp(int(resp[j][0] / 1000)))
+    # time_list.pop(671)  # because the first item of the first list is absent: the last is doubled. This corrects.
+    pair_list.pop(671)  # because the first item of the first list is absent: the last is doubled. This corrects.
     return pair_list
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from func import *
 import pandas as pd
 import numpy as np
+from datetime import datetime
 
 '''
 Safe 2 years of hourly coin-market prices per coin. 
 '''
 
-# coin_list = ['BTC', 'ETH', 'SOL', 'ADA', 'MATIC', 'AVAX', 'SAND']
-coin_list = ['BTC']
+coin_list = ['BTC', 'ETH', 'SOL', 'ADA', 'MATIC', 'AVAX', 'SAND']
 coin_price_lst = np.array([price_list(each) for each in coin_list])
-pd.DataFrame.from_records(coin_price_lst, index=coin_list).T.to_csv('name.csv')
-print(pd.DataFrame.from_records(coin_price_lst, index=coin_list).T)
+
+
+# safe to file
+name = 'name-' + str(int(datetime.timestamp(datetime.now()))) + '.csv'
+pd.DataFrame.from_records(coin_price_lst, index=coin_list).T.to_csv(name)
