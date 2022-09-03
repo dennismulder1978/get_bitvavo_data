472448a532cdba143345e5624e2841b123113480
Dennis Mulder <dnns.mldr@gmail.com>
2022-09-03 15:04:53 +0200

    Expansion to 2 years! v1.1 Filled with 0.0 when data is absent.

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ Save 2 years of hourly coin-market prices per coin.
 coin_list = ['BTC', 'ETH', 'SOL', 'ADA', 'MATIC', 'AVAX', 'SAND']
 coin_price_lst = np.array([price_list(each) for each in coin_list])
 
-print(coin_price_lst)
 # safe to file
 name = 'CoinPriceData__' + datetime.now().strftime('%d-%m-%Y_%H.%M.%S') + '.csv'
-# pd.DataFrame.from_records(coin_price_lst, index=coin_list).T.to_csv(name)
+pd.DataFrame.from_records(coin_price_lst, index=coin_list).T.to_csv(name)
