359ca8cf20a1bd454acee21811d323a9bfff6c2a
Dennis Mulder <dnns.mldr@gmail.com>
2023-08-03 20:24:14 +0200

    Expansion to 2 years! v1.1 Filled with 0.0 when data is absent.

--- func.py
+++ func.py
@@ -35,7 +35,7 @@ def cohort_creator(period: str= '1d'):
     for i in range(0, ceil(133574400000 / timer)):  # 133... is 1546 days = approximately total amount of data available
         x = now_timestamp_in_milliseconds - (i * timer)
         time_list.append(x)
-    print(f'Number of candle cohorts:{len(time_list)}')
+    print(f'    Number of candle cohorts: {len(time_list)}..')
     return time_list
 
 def price_list(symbol: str = 'ETH', period: str = '1d', go: bool=True):
@@ -46,30 +46,29 @@ def price_list(symbol: str = 'ETH', period: str = '1d', go: bool=True):
     coin_values_list = []
 
     try:
+        print('    Start trying to get prices...', end="")
         for i in range(0, len(time_list)):
+            print(f' {i + 1}', end="")
             resp = bitvavo.candles(pair, period, {'limit': 1000, 'end': time_list[i]})
-            print(resp)
             for each in resp:
                 each[0] = int(each[0]/1000)
                 if i > 0:
-                    if each[0] in datetime_list:
-                        print('datetime dubbel!!')
-                    else:
+                    if each[0] not in datetime_list:
                         coin_values_list.append(each)
                 else:
                     coin_values_list.append(each)
                 datetime_list.append(each[0])
-            print(i)
     except Exception as e:
         print(e)
         go = False
+    print('....Done..')
     return go, coin_values_list
 
 
 def save_to_file(list_data: list, column_list: list, file_name: str = 'file'):
     df = pd.DataFrame(list_data)
     path = './results/'
-    name_of_file = path + file_name + f'__created_{datetime.now().strftime("%d-%b-%Y")}.csv'
+    name_of_file = path + file_name + f'__created_{datetime.now().strftime("%d-%b-%Y_%H-%M")}.csv'
     df.to_csv(name_of_file, index=False, header=column_list)
-
+    print(f'    File saved. {name_of_file}..')
     return name_of_file + ' saved.'
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from func import *
 
 coins_list = ['LUNA2']
-periods_list = ['5m', '1h', '2h', '3h', '6h', '12h', '1d', '2d']
+periods_list = ['1h']#['5m', '1h', '2h', '3h', '6h', '12h', '1d', '2d']
 column_list = ['Date', 'Open', 'High', 'Low', 'Close', 'Vol.']
 
 for coin in coins_list:
     for period in periods_list:
-        print(f'{coin}::{period}')
+        print(f'{coin}..{period}:')
         go_signal, price_list = price_list(symbol=coin, period=period, go=True)
         if go_signal:
             filename = f'{coin}_trade_data__per_{period}__{len(price_list)}_items'
             save_to_file(list_data=price_list, column_list=column_list, file_name=filename)
+print('\n__DONE__')
