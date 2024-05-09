9c9e7789f7f58bd8d4c0f095172b34d89a5650e5
Dennis <dnns.mldr@gmail.com>
2024-05-09 21:05:28 +0200

    testing

--- testing.py
+++ testing.py
@@ -22,40 +22,33 @@ def create_df(shift:int=2):
     LUNA2["change_perc"] = ((LUNA2["Target"] - LUNA2["Close"])/LUNA2["Close"]) * 100
     LUNA2["Vol_adjusted"] = (LUNA2["Vol."] / LUNA2["Vol."].mean()) / 100
     LUNA2 = LUNA2.dropna()
-    LUNA2 = LUNA2.drop(["Date", "Vol."], axis=1)
+    LUNA2 = LUNA2.drop(["Open", "High", "Low", "Date", "Vol.", "change", "Vol_adjusted", "Target"], axis=1)
 
     return LUNA2
 
 
-def cointest(df: pd.DataFrame, sell_perc:int=-1, buy_perc:int=1):    
+def coin_test(df: pd.DataFrame, sell_perc:int=-1, buy_perc:int=1):    
     coin = 0
     euro = 100
     counter = 0
-    for each in range(df.shape[1]):
-        if (df.iloc[each, 6] > buy_perc) & (coin == 0):
-            print(euro)
-            coin = euro / df.iloc[each, 3]
-            print(coin)
-            print(df.iloc[each])
-            print(df.iloc[each, 3])
+    for each in range(df.shape[0]):
+        if (df.iloc[each, 1] > buy_perc) & (coin == 0):
+            coin = euro / df.iloc[each, 0]
             euro = 0
             counter += 1
-        elif (df.iloc[each, 6] < sell_perc) & (euro == 0):
-            print(f'sell: {df.iloc[each, 3]} {coin}')
-            euro = df.iloc[each, 3] * coin
+        elif (df.iloc[each, 1] < sell_perc) & (euro == 0):
+            euro = df.iloc[each, 1] * coin
             coin = 0
 
     print(f'Counter: {counter}')
     if euro == 0:
-        euro = df.iloc[-1, 3] * coin
-    print(df.iloc[-1, 3])
+        euro = df.iloc[-1, 0] * coin
     return euro
     
 
-# for i in range(100,113,6):
-#     print(f'Shift {float(i/12)} uur: {test(i)}')
+for i in range(120,140,6):
+    print(f'Shift {float(i/12)} uur: {coin_test(df=create_df(shift=i), sell_perc=-3, buy_perc=3)}')
 
-df = create_df(2)
-print(f'Shift uur: {cointest(df=df)}')
+
 
 # adding target price
