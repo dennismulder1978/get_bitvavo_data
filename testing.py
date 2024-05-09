e0f4a32865d8a40fd5e65adfac015912c6259226
Dennis <dnns.mldr@gmail.com>
2024-05-09 21:56:39 +0200

    afa

--- testing.py
+++ testing.py
@@ -37,18 +37,42 @@ def coin_test(df: pd.DataFrame, sell_perc:int=-1, buy_perc:int=1):
             euro = 0
             counter += 1
         elif (df.iloc[each, 1] < sell_perc) & (euro == 0):
-            euro = df.iloc[each, 1] * coin
+            euro = df.iloc[each, 0] * coin
             coin = 0
-
-    print(f'Counter: {counter}')
+    
+    # final result in Euro
     if euro == 0:
         euro = df.iloc[-1, 0] * coin
+    
     return euro
     
+end_result = []
+index_future = []
+index_buy_prec = []
+index_sell_prec = []
 
-for i in range(120,140,6):
-    print(f'Shift {float(i/12)} uur: {coin_test(df=create_df(shift=i), sell_perc=-3, buy_perc=3)}')
+for i in range(12,121,6):
+    index_future.append(i)
+    print(f"|{i}|", end="")
+    luna2_dataframe = create_df(shift=i)
+    # luna2_dataframe = luna2_dataframe[:100]
+    result_j = []
+    for jj in range(2,22,1):
+        print(".", end="")
+        j = jj/2
+        index_buy_prec.append(j)
+        result_k = []
+        for kk in range(-2,-22,-1):
+            k = kk/2
+            index_sell_prec.append(k)
+            result_k.append(coin_test(df=luna2_dataframe, sell_perc=k, buy_perc=j))
+        result_j.append(result_k)
+    end_result.append(result_j)
 
+print("\n--------------------------")
+end_result_array = np.array(end_result)
 
+np.save('end_result_array', end_result_array)
 
-# adding target price
+max = np.unravel_index(end_result_array.argmax(), end_result_array.shape)
+print(f'Max result: Euro:{end_result_array.max():.2f}, cohort: {index_future[max[0]]}, buy percentage: {index_buy_prec[max[1]]}, sell percentage: {index_sell_prec[max[2]]}')
